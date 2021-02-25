6e871a5828c19483dbb242002460d6042a699cc8
iiizadora <69588653+iiizadora@users.noreply.github.com>
2021-02-25 16:23:42 -0300

    commit -m

---
+++ src/pages/index.tsx
@@ -0,0 +1,36 @@
+import Head from 'next/head';
+
+
+import { Profile } from "../components/Profile";
+import { ExperienceBar} from "../components/ExperienceBar";
+import { CompletedChallenges } from "../components/CompletedeChallenges";
+import { Countdown } from "../components/Countdown";
+import { ChallengeBox } from '../components/ChallengeBox';
+
+import styles from '../styles/pages/Home.module.css';
+
+
+export default function Home() {
+  return (
+    <div className= {styles.container}>
+
+      <Head>
+        <title>Inicio | move.it</title>
+      </Head>
+      <ExperienceBar/>
+
+      <section>
+        <div>
+          < Profile />
+          <CompletedChallenges />
+          <Countdown />
+
+        </div>
+        <div>
+          <ChallengeBox/>
+
+        </div>
+      </section>
+    </div>
+  )
+}
